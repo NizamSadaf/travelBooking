04956dd167a7d3c7b04da1f1bddb2d9c2e9644bd
Nizam Uddin Ahmed <45719824+NizamSadaf@users.noreply.github.com>
2023-09-30 15:44:13 +0600

    Update index.js

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ const app = express()
 
 dotenv.config() 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://nizam-travel-booking.vercel.app');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, authentication');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
