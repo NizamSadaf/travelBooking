18b738c97d31dff7492ffc40fbaf76dce404f28d
Nizam Uddin Ahmed <45719824+NizamSadaf@users.noreply.github.com>
2023-09-30 15:16:05 +0600

    Update index.js

--- server/index.js
+++ server/index.js
@@ -51,9 +51,10 @@ mongoose.connect(process.env.MONGO_CONNECTION_STRING,
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'https://nizam-travel-booking.vercel.app');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, authentication');
   next();
 });
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
